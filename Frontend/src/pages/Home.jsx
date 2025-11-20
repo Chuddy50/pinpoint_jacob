074b1fadf488407792b48d8001a752dd4e0c675c
Jacob Nguyen <jacobnguyen7@gmail.com>
2025-11-19 17:40:09 -0700

    updated to grid layout

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -3,11 +3,14 @@ import CompanyList from "../components/CompanyList";
 
 export default function Home() {
   return (
-    
-    // color of background 
-    <div className="flex w-screen min-h-screen bg-[#F7F7F7]">
-      <NavBar/>
-     <CompanyList/>
+    <div className="flex min-h-screen w-full bg-[#F3F4F6] p-6 gap-6">
+      <aside className="w-72">
+        <NavBar />
+      </aside>
+
+      <div className="flex-1 rounded-3xl bg-white p-10 shadow-sm">
+        <CompanyList />
+      </div>
     </div>
   );
 }
